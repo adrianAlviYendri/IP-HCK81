b70a65a4acf713bd03be2333aca38a2d65ddeb97
adrian alvi yendri <adrianalviyendri@gmail.com>
2025-03-25 14:02:40 +0700

    chore: create migrations and models

---
+++ server/models/movie.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Movie.belongsToMany(models.User, {
+        through: models.Favorite,
+        foreignKey: "MovieId",
+        otherKey: "UserId",
+      });
+    }
+  }
+  Movie.init(
+    {
+      title: DataTypes.STRING,
+      overview: DataTypes.TEXT,
+      posterPath: DataTypes.STRING,
+      releaseDate: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Movie",
+    }
+  );
+  return Movie;
+};
